ad8a3ba4df1cfa63e4e171180af903e7e6114871
devfsc <30779666+devfsc@users.noreply.github.com>
2018-03-11 13:13:21 -0600

    Superfical modification for languages and main colors

--- src/culture/es.js
+++ src/culture/es.js
@@ -2,26 +2,26 @@
 	translator.translations["es"] = {
 		// javascript alerts or messages
 		"testneteditionactivated": "Testnet se activa",
-		"paperlabelbitcoinaddress": "Dirección $Pac:",
+		"paperlabelbitcoinaddress": "Dirección $PAC:",
 		"paperlabelprivatekey": "Clave privada:",
 		"paperlabelencryptedkey": "Clave privada cifrada (contraseña necesaria)",
 		"bulkgeneratingaddresses": "Generación de direcciones... ",
 		"brainalertpassphrasetooshort": "La contraseña introducida es demasiado corta.\n\n",
-		"brainalertpassphrasewarning": "Aviso: Es importante escoger una contraseña fuerte para evitar ataques de fuerza bruta a fin de adivinarla y robar tus bitcoins.",
+		"brainalertpassphrasewarning": "Aviso: Es importante escoger una contraseña fuerte para evitar ataques de fuerza bruta a fin de adivinarla y robar tus $PAC.",
 		"brainalertpassphrasedoesnotmatch": "Las contraseñas no coinciden.",
 		"detailalertnotvalidprivatekey": "El texto que has introducido no es una clave privada válida",
-		"detailconfirmsha256": "El texto que has introducido no es una clave privada válida\n\n¿Quieres usar ese texto como si fuera una contraseña y generar una clave privada usando un hash SHA256 de tal contraseña?\n\nAviso: Es importante escoger una contraseña fuerte para evitar ataques de fuerza bruta a fin de adivinarla y robar tus bitcoins.",
+		"detailconfirmsha256": "El texto que has introducido no es una clave privada válida\n\n¿Quieres usar ese texto como si fuera una contraseña y generar una clave privada usando un hash SHA256 de tal contraseña?\n\nAviso: Es importante escoger una contraseña fuerte para evitar ataques de fuerza bruta a fin de adivinarla y robar tus $PAC.",
 		"bip38alertincorrectpassphrase": "Incorrect passphrase for this encrypted private key.", //TODO: please translate
 		"bip38alertpassphraserequired": "Passphrase required for BIP38 key", //TODO: please translate
 		"vanityinvalidinputcouldnotcombinekeys": "Entrada no válida. No se puede combinar llaves.",
 		"vanityalertinvalidinputpublickeysmatch": "Entrada no válida. La clave pública de ambos coincidan entradas. Debe introducir dos claves diferentes.",
-		"vanityalertinvalidinputcannotmultiple": "Entrada no válida. No se puede multiplicar dos claves públicas. Seleccione 'Añadir' para agregar dos claves públicas para obtener una dirección bitcoin.",
+		"vanityalertinvalidinputcannotmultiple": "Entrada no válida. No se puede multiplicar dos claves públicas. Seleccione 'Añadir' para agregar dos claves públicas para obtener una dirección $PAC.",
 		"vanityprivatekeyonlyavailable": "Sólo está disponible cuando se combinan dos claves privadas",
 		"vanityalertinvalidinputprivatekeysmatch": "Entrada no válida. La clave privada de ambos coincidan entradas. Debe introducir dos claves diferentes.",
 
 		// header and menu html
-		"tagline": "Generador de carteras $Pac de código abierto en lado de cliente con Javascript",
-		"generatelabelbitcoinaddress": "Generando dirección $Pac...",
+		"tagline": "Generador de carteras $PAC de código abierto en lado de cliente con Javascript",
+		"generatelabelbitcoinaddress": "Generando dirección $PAC...",
 		"generatelabelmovemouse": "Mueve un poco el ratón para crear entropía...",
 		"generatelabelkeypress": "OR type some random characters into this textbox", //TODO: please translate
 		"singlewallet": "Una sola cartera",
@@ -65,22 +65,22 @@
 		"statuslabelprotocolbad2": "download", //TODO: please translate
 		"statuslabelprotocolbad3": "the zip file from GitHub and run this generator offline as a local html file.", //TODO: please translate
 		"statusokprotocolbad": "OK", //TODO: please translate
-		"statuslabelkeypool1": "This is a log of all the $Pac Addresses and Private Keys you generated during your current session. Reloading the page will create a new session.", //TODO: please translate
+		"statuslabelkeypool1": "This is a log of all the $PAC Addresses and Private Keys you generated during your current session. Reloading the page will create a new session.", //TODO: please translate
 		"statuskeypoolrefresh": "Refresh", //TODO: please translate
 		"statusokkeypool": "OK", //TODO: please translate
 
 		// single wallet html
 		"newaddress": "Generar dirección",
 		"singleprint": "Imprimir",
-		"singlelabelbitcoinaddress": "Dirección $Pac",
+		"singlelabelbitcoinaddress": "Dirección $PAC",
 		"singlelabelprivatekey": "Clave privada (formato para importar):",
-		"singletip1": "<b>A $Pac wallet</b> is as simple as a single pairing of a $Pac address with it's corresponding $Pac private key. Such a wallet has been generated for you in your web browser and is displayed above.", //TODO: please translate
-		"singletip2": "<b>To safeguard this wallet</b> you must print or otherwise record the $Pac address and private key. It is important to make a backup copy of the private key and store it in a safe location. This site does not have knowledge of your private key. If you are familiar with PGP you can download this all-in-one HTML page and check that you have an authentic version from the author of this site by matching the SHA256 hash of this HTML with the SHA256 hash available in the signed version history document linked on the footer of this site. If you leave/refresh the site or press the Generate New Address button then a new private key will be generated and the previously displayed private key will not be retrievable.	Your $Pac private key should be kept a secret. Whomever you share the private key with has access to spend all the bitcoins associated with that address. If you print your wallet then store it in a zip lock bag to keep it safe from water. Treat a paper wallet like cash.", //TODO: please translate
-		"singletip3": "<b>Add funds</b> to this wallet by instructing others to send bitcoins to your $Pac address.", //TODO: please translate
-		"singletip4": "<b>Check your balance</b> by going to blockchain.info or blockexplorer.com and entering your $Pac address.", //TODO: please translate
-		"singletip5": "<b>Spend your bitcoins</b> by going to blockchain.info and sweep the full balance of your private key into your account at their website. You can also spend your funds by downloading one of the popular bitcoin p2p clients and importing your private key to the p2p client wallet. Keep in mind when you import your single key to a bitcoin p2p client and spend funds your key will be bundled with other private keys in the p2p client wallet. When you perform a transaction your change will be sent to another bitcoin address within the p2p client wallet. You must then backup the p2p client wallet and keep it safe as your remaining bitcoins will be stored there. Satoshi advised that one should never delete a wallet.", //TODO: please translate
-		"singleshare": "SHARE", //TODO: please translate
-		"singlesecret": "SECRET", //TODO: please translate
+		"singletip1": "<b>Una cartera de $PAC</b> es tan simple como un emparejamiento simple de una dirección $PAC con su correspondiente clave privada. Tal cartera ha sido generada para usted en su navegador web y se muestra arriba.", //TODO: please translate
+		"singletip2": "<b>Para proteger esta cartera</b>debe imprimir o guardar la dirección $PAC y la clave privada.  Es importante hacer una copia de seguridad de la clave privada y almacenarla en un lugar seguro.  Este sitio no tiene conocimiento de su clave privada. Si abandona / actualiza el sitio o presiona el botón Generar nueva dirección, se generará una nueva clave privada y no se podrá recuperar la clave privada que se mostró anteriormente. Su clave privada de $ Pac debe mantenerse en secreto. Quien comparte la clave privada tiene acceso para gastar todos los $PAC asociados con esa dirección.  Si imprime su cartera, guárdela en una bolsa con cierre hermético para mantenerla a salvo del agua. Trate una cartera de papel como efectivo.", //TODO: please translate
+		"singletip3": "<b>Agregue fondos </ b> a este monedero instruyendo a otros a enviar $PAC a su dirección de $PAC.", //TODO: please translate
+		"singletip4": "<b>Verifica tu balance</b> introduciendo tu direccion de $PAC en http://usa.pacblockexplorer.com:3002/ ", //TODO: please translate
+		"singletip5": "También puede gastar sus fondos descargando uno de los populares clientes de $PAC p2p e importando su clave privada al monedero de cliente p2p. Tenga en cuenta que cuando importe su clave individual a un cliente bitcoin p2p y gaste fondos, su clave se incluirá con otras claves privadas en la cartera del cliente p2p. Cuando realiza una transacción, su cambio se enviará a otra dirección de $PAC dentro del monedero de cliente p2p. A continuación, debe hacer una copia de seguridad del monedero del cliente p2p y mantenerlo a salvo ya que sus $PAC restantes se almacenarán allí. Satoshi aconsejó que nunca se debe eliminar una cartera.", //TODO: please translate
+		"singleshare": "PUBLICA", //TODO: please translate
+		"singlesecret": "SECRETA", //TODO: please translate
 
 		// paper wallet html
 		"paperlabelhideart": "Ocultar diseño",
@@ -103,14 +103,14 @@
 		"bulkprint": "Imprimir",
 		"bulklabelcsv": "Valores separados por coma:",
 		"bulklabelformat": "Índice,Dirección,Clave privada (formato para importar)",
-		"bulklabelq1": "¿Por qué debo usar \"Direcciones en masa\" para aceptar $Pacs en mi web?",
-		"bulka1": "La forma tradicional de aceptar bitcoins en tu web requiere tener instalado el cliente oficial de bitcoin (\"bitcoind\"). Sin embargo muchos servicios de hosting no permiten instalar dicho cliente. Además, ejecutar el cliente en tu servidor supone que las claves privadas están también en el servidor y podrían ser comprometidas en caso de intrusión. Al usar este mecanismo, puedes subir al servidor sólo las dirección de bitcoin y no las claves privadas. De esta forma no te tienes que preocupar de que alguien robe la cartera si se cuelan en el servidor.",
-		"bulklabelq2": "¿Cómo uso \"Direcciones en masa\" para aceptar bitcoins en mi web?",
+		"bulklabelq1": "¿Por qué debo usar \"Direcciones en masa\" para aceptar $PACs en mi web?",
+		"bulka1": "La forma tradicional de aceptar $PAC en tu web requiere tener instalado el cliente oficial de $PAC (\"paccoind\"). Sin embargo muchos servicios de hosting no permiten instalar dicho cliente. Además, ejecutar el cliente en tu servidor supone que las claves privadas están también en el servidor y podrían ser comprometidas en caso de intrusión. Al usar este mecanismo, puedes subir al servidor sólo las dirección de $PAC y no las claves privadas. De esta forma no te tienes que preocupar de que alguien robe la cartera si se cuelan en el servidor.",
+		"bulklabelq2": "¿Cómo uso \"Direcciones en masa\" para aceptar $PAC en mi web?",
 		"bulklabela2li1": "Usa el tab \"Direcciones en masa\" para generar por anticipado muchas direcciones (más de 10000). Copia y pega la lista de valores separados por comas (CSV) a un archivo de texto seguro (cifrado) en tu ordenador. Guarda una copia de seguridad en algún lugar seguro.",
 		"bulklabela2li2": "Importa las direcciones en la base de datos de tu servidor. No subas la cartera ni las claves públicas, o de lo contrario te lo pueden robar. Sube sólo las direcciones, ya que es lo que se va a mostrar a los clientes.",
-		"bulklabela2li3": "Ofrece una alternativa en el carro de la compra de tu web para que los clientes paguen con $Pac. Cuando el cliente elija pagar con $Pac, les muestras una de las direcciones de la base de datos como su \"dirección de pago\" y guardas esto junto con el pedido.",
-		"bulklabela2li4": "Ahora te hace falta recibir una notificación del pago. Busca en google \"notificación de pagos bitcoin\" (o \"bitcoin payment notification\" en inglés) y suscríbete a alguno de los servicios que aparezcan. Hay varios de ellos, que te pueden notificar vía Web services, API, SMS, email, etc. Una vez te llegue la notificación, lo cual puede ser automatizado, entonces ya puedes procesar el pedido. Para comprobar a mano si has recibido un pago, puedes usar Block Explorer: reemplaza DIRECCION a continuación por la dirección que estés comprobando. La transacción puede tardar entre 10 minutos y una hora en ser confirmada. <br />http://www.blockexplorer.com/address/DIRECCION<br /><br />Puedes ver las transacciones sin confirmar en: http://blockchain.info/ <br />Las transacciones sin confirmar suelen aparecer ahí en unos 30 segundos.",
-		"bulklabela2li5": "Las bitcoins que recibas se almacenarán de forma segura en la cadena de bloques. Usa la cartera original que generaste en el paso 1 para usarlas.",
+		"bulklabela2li3": "Ofrece una alternativa en el carro de la compra de tu web para que los clientes paguen con $PAC. Cuando el cliente elija pagar con $PAC, les muestras una de las direcciones de la base de datos como su \"dirección de pago\" y guardas esto junto con el pedido.",
+		"bulklabela2li4": "Para comprobar a mano si has recibido un pago, puedes usar Block Explorer: reemplaza DIRECCION a continuación por la dirección que estés comprobando. La transacción puede tardar entre 10 minutos y una hora en ser confirmada. <br />http://usa.pacblockexplorer.com:3002/DIRECCION<br /><br />Puedes ver las transacciones sin confirmar en: http://usa.pacblockexplorer.com:3002/ <br />Las transacciones sin confirmar suelen aparecer ahí en unos 30 segundos.",
+		"bulklabela2li5": "Las $PAC que recibas se almacenarán de forma segura en la cadena de bloques. Usa la cartera original que generaste en el paso 1 para usarlas.",
 
 		// brain wallet html
 		"brainlabelenterpassphrase": "Contraseña:",
@@ -119,7 +119,7 @@
 		"brainlabelconfirm": "Confirmar contraseña:",
 		"brainview": "Ver",
 		"brainalgorithm": "Algoritmo: SHA256(contraseña)",
-		"brainlabelbitcoinaddress": "Dirección $Pac:",
+		"brainlabelbitcoinaddress": "Dirección $PAC:",
 		"brainlabelprivatekey": "Clave privada (formato para importar):",
 
 		// vanity wallet html
@@ -137,7 +137,7 @@
 		"vanitylabelradioadd": "Añadir",
 		"vanitylabelradiomultiply": "Multiplicar",
 		"vanitycalc": "Calcular cartera personalizada",
-		"vanitylabelbitcoinaddress": "Dirección $Pac personalizada:",
+		"vanitylabelbitcoinaddress": "Dirección $PAC personalizada:",
 		"vanitylabelnotesbitcoinaddress": "Esta es tu nueva dirección, que debería tener el prefijo deseado.",
 		"vanitylabelpublickeyhex": "Clave pública personalizada (HEX):",
 		"vanitylabelnotespublickeyhex": "Lo anterior es la clave pública en formato hexadecimal.",
@@ -150,9 +150,9 @@
 		"detailview": "Ver detalles",
 		"detailprint": "Imprimir",
 		"detaillabelnote1": "Tu clave privada es un número secreto, único, que sólo tú conoces. Se puede expresar en varios formatos. Aquí abajo mostramos la dirección y la clave pública que se corresponden con tu clave privada, así como la clave privada en los formatos más conocidos (para importar, hex, base64 y mini).",
-		"detaillabelnote2": "$Pac v0.6+ almacena las claves públicas comprimidas. El cliente también soporta importar/exportar claves privadas usando importprivkey/dumpprivkey. El formato de las claves privadas exportadas depende de si la dirección se generó en una cartera antigua o nueva.",
-		"detaillabelbitcoinaddress": "Dirección $Pac:",
-		"detaillabelbitcoinaddresscomp": "Dirección $Pac (comprimida):",
+		"detaillabelnote2": "$PAC v0.6+ almacena las claves públicas comprimidas. El cliente también soporta importar/exportar claves privadas usando importprivkey/dumpprivkey. El formato de las claves privadas exportadas depende de si la dirección se generó en una cartera antigua o nueva.",
+		"detaillabelbitcoinaddress": "Dirección $PAC:",
+		"detaillabelbitcoinaddresscomp": "Dirección $PAC (comprimida):",
 		"detaillabelpublickey": "Clave pública (130 caracteres [0-9A-F]):",
 		"detaillabelpublickeycomp": "Clave pública (comprimida, 66 caracteres [0-9A-F]):",
 		"detaillabelprivwif": "Clave privada para importar (51 caracteres en base58, empieza con un",
@@ -164,7 +164,7 @@
 		"detaillabelpassphrase": "BIP38 Passphrase", //TODO: please translate
 		"detailbip38decryptbutton": "Decrypt BIP38", //TODO: please translate
 		"detailbip38encryptbutton": "Encrypt BIP38", //TODO: please translate
-		"detaillabelq1": "How do I make a wallet using dice? What is B6?", //TODO: please translate
-		"detaila1": "An important part of creating a $Pac wallet is ensuring the random numbers used to create the wallet are truly random. Physical randomness is better than computer generated pseudo-randomness. The easiest way to generate physical randomness is with dice. To create a $Pac private key you only need one six sided die which you roll 99 times. Stopping each time to record the value of the die. When recording the values follow these rules: 1=1, 2=2, 3=3, 4=4, 5=5, 6=0. By doing this you are recording the big random number, your private key, in B6 or base 6 format. You can then enter the 99 character base 6 private key into the text field above and click View Details. You will then see the $Pac address associated with your private key. You should also make note of your private key in WIF format since it is more widely used." //TODO: please translate
+		"detaillabelq1": "¿Cómo hago una billetera usando dados? Qué es B6?", //TODO: please translate
+		"detaila1": "Una parte importante de la creación de una billetera $ PAC es garantizar que los números aleatorios utilizados para crear la billetera sean verdaderamente aleatorios. La aleatoriedad física es mejor que la pseudoaleatoriedad generada por computadora. La forma más fácil de generar aleatoriedad física es con dados. Para crear una clave privada $ PAC, solo necesita un dado de seis caras que se tira 99 veces. Detener cada vez para registrar el valor del dado. Al registrar los valores, siga estas reglas: 1 = 1, 2 = 2, 3 = 3, 4 = 4, 5 = 5, 6 = 0. Al hacer esto, está grabando el gran número aleatorio, su clave privada, en formato B6 o base 6. Luego puede ingresar la clave privada de la base 6 de 99 caracteres en el campo de texto de arriba y hacer clic en Ver detalles. Luego verá la dirección $ PAC asociada con su clave privada. También debe tomar nota de su clave privada en formato WIF ya que es más ampliamente utilizada." //TODO: please translate
 	};
 })(ninja.translator);
